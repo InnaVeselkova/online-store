33447e63fdb70a1098967203134644fe5ff624e9
Inna <MenelN@yandex.ru>
2025-08-04 19:11:18 +0300

    update

--- src/product.py
+++ src/product.py
@@ -9,11 +9,31 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def from_dict(cls, data: Dict) -> 'Product':
+        return cls(
+            name=data.get('name'),
+            description=data.get('description'),
+            price=float(data.get('price')),
+            quantity=int(data.get('quantity'))
+        )
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @price.setter
+    def price(self, value: float):
+        if value <= 0:
+            print("Цена не должна быть нулевой или отрицательной")
+        else:
+            self._price = value
 
 
 def load_product_json(path: str) -> List[Dict]:
