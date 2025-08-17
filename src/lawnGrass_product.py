780d8dac93f6f89ce8d0af39cd5c039d8681b957
Inna <MenelN@yandex.ru>
2025-08-17 16:58:06 +0300

    add correction

--- src/lawnGrass_product.py
+++ src/lawnGrass_product.py
@@ -2,13 +2,13 @@ from src.product import Product
 
 
 class LawnGrass(Product):
-    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str, color: str ):
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str, color: str):
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
         self.color = color
 
-   def __add__(self, other):
+    def __add__(self, other):
         # Складывает стоимость всех продуктов
         if type(other) is LawnGrass:
             return self.price * self.quantity + other.price * other.quantity
